a9fbf5346ff65be650564b0f1d180b77a23bf4b2
Panpan <30294137+PanpanZheng@users.noreply.github.com>
2018-01-31 17:08:34 -0600

    Add files via upload

---
+++ Baselines/run_baseline.m
@@ -0,0 +1,144 @@
+function [precision_neg, recall_neg, f1_neg, accuracy] = run_baseline(file_url,NDtype,en_ae)
+
+% type_list = ['dist', 'nn', 'kmeans', 'parzen', 'gmm', 'svmTax', 'gpoc', 'kde', 'som', 'pca', 'kpca'];
+
+data = load(file_url);
+X = data.x; 
+y = data.y;
+
+%% Sampling training, validating, and testing set.
+
+isnor = y == 1; % regard class 1 as normal.
+isab = ~isnor;
+[traindataNorOri, validdataNorOri, testdataNorOri, validdataAbOri, testdataAbOri] = splitData(X,isab,en_ae);
+
+size(traindataNorOri, 1); 
+size(testdataNorOri, 1); 
+size(testdataAbOri, 1); 
+size(validdataNorOri, 1); 
+size(validdataAbOri, 1);
+
+%% Training, validating, and testing. 
+
+switch lower(NDtype)
+    case 'gpoc'
+        trained_model = train_gpoc(traindataNorOri);
+    case 'svmsch' 
+        trained_model = train_svmsch(traindataNorOri);
+    case 'nn'
+        trained_model = train_nn(traindataNorOri);
+    case 'kpca'
+        trained_model = train_kpca(traindataNorOri);
+    case 'svmtax'
+        trained_model = train_svmtax(traindataNorOri);
+    case 'pca'
+        trained_model = train_pca(traindataNorOri);
+    case 'kde'
+        trained_model = train_kde(traindataNorOri);
+end
+
+
+%% Testing
+
+switch lower(NDtype)
+    case 'gpoc'
+        output_nor = out_gpoc(testdataNorOri, trained_model);
+        output_ab = out_gpoc(testdataAbOri, trained_model);
+    case 'svmsch'
+        output_nor = out_svmsch(testdataNorOri, trained_model);
+        output_ab = out_svmsch(testdataAbOri, trained_model);
+    case 'nn'
+        output_nor = out_nn(testdataNorOri, trained_model);
+        output_ab = out_nn(testdataAbOri, trained_model);
+    case 'kpca'
+        output_nor = out_kpca(testdataNorOri, trained_model);
+        output_ab = out_kpca(testdataAbOri, trained_model);
+    case 'svmtax'
+        output_nor = out_svmtax(testdataNorOri, trained_model);
+        output_ab = out_svmtax(testdataAbOri, trained_model);
+    case 'pca'
+        output_nor = out_pca(testdataNorOri, trained_model);
+        output_ab = out_pca(testdataAbOri, trained_model);
+    case 'kde'
+        output_nor = out_kde(testdataNorOri, trained_model);
+        output_ab = out_kde(testdataAbOri, trained_model);
+end
+
+
+%% Validation (threshold)
+
+
+switch lower(NDtype)
+    case 'gpoc'
+        [~, optthr] = minErr_thr(trained_model, validdataNorOri, validdataAbOri, 'gpoc'); 
+    case 'svmsch'
+        [~, optthr] = minErr_thr(trained_model, validdataNorOri, validdataAbOri, 'svmsch');
+    case 'nn'
+        [~, optthr] = minErr_thr(trained_model, validdataNorOri, validdataAbOri, 'nn'); 
+    case 'kpca'
+        [~, optthr] = minErr_thr(trained_model, validdataNorOri, validdataAbOri, 'kpca');
+    case 'svmtax'
+        [~, optthr] = minErr_thr(trained_model, validdataNorOri, validdataAbOri, 'svmtax'); 
+    case 'pca'
+        [~, optthr] = minErr_thr(trained_model, validdataNorOri, validdataAbOri, 'pca');
+     case 'kde'
+        [~, optthr] = minErr_thr(trained_model, validdataNorOri, validdataAbOri, 'kde'); 
+end 
+
+
+%% Get label 
+switch lower(NDtype)
+    case 'gpoc'
+        pred_nor = assignCls('gpoc', output_nor, optthr);
+        pred_ab = assignCls('gpoc', output_ab, optthr);
+    case 'svmsch'
+        pred_nor = assignCls('svmsch', output_nor, optthr);
+        pred_ab = assignCls('svmsch', output_ab, optthr);
+    case 'nn'        
+        pred_nor = assignCls('nn', output_nor, optthr);
+        pred_ab = assignCls('nn', output_ab, optthr);
+    case 'kpca'
+        pred_nor = assignCls('kpca', output_nor, optthr);
+        pred_ab = assignCls('kpca', output_ab, optthr);
+    case 'svmtax'
+        pred_nor = assignCls('svmtax', output_nor, optthr);
+        pred_ab = assignCls('svmtax', output_ab, optthr);
+    case 'pca'
+        pred_nor = assignCls('pca', output_nor, optthr);
+        pred_ab = assignCls('pca', output_ab, optthr);
+    case 'kde'
+        pred_nor = assignCls('kde', output_nor, optthr);
+        pred_ab = assignCls('kde', output_ab, optthr);
+end
+
+pred_labels = [pred_nor; pred_ab];
+
+
+
+%% Compute confusion matrix of test data.
+
+tar_nor = zeros(size(output_nor, 1), 1);
+tar_ab = ones(size(output_ab, 1), 1);
+tar_labels = [tar_nor; tar_ab];
+
+
+[conf, ~] = confmat(pred_labels, tar_labels); % predTest and tarTest are 0-1 coding.
+fprintf('\n');
+disp(conf);
+fprintf('\n');
+% fprintf('Confusion matrix using test data is:\n');
+% disp(conf);
+accuracy = (conf(1,1) + conf(2,2)) / sum(conf(:)); % accuracy = rate(1)
+
+precision_pos = conf(1,1)/(conf(1,1) + conf(2,1)); 
+precision_neg = conf(2,2)/(conf(1,2) + conf(2,2));
+precision = (precision_pos + precision_neg)/2; 
+
+recall_pos = conf(1,1)/(conf(1,1) + conf(1,2)); 
+recall_neg = conf(2,2)/(conf(2,1) + conf(2,2));
+recall = (recall_pos + recall_neg)/2;
+
+f1_pos = 2 * ((precision_pos*recall_pos)/(precision_pos + recall_pos)); 
+f1_neg = 2 * ((precision_neg*recall_neg)/(precision_neg + recall_neg));
+
+f1 = (f1_pos + f1_neg)/2;
---
+++ Baselines/splitData.m
@@ -0,0 +1,108 @@
+function [traindataNor, validdataNor, testdataNor, validdataAb, testdataAb] = splitData(alldata, isab, en_ae)
+%SPLITDATA Split data into three groups: training, validation, and test set.
+%   [traindataNor, validdataNor, testdataNor, validdataAb, testdataAb] = splitData(alldata, isab)
+%
+%   Inputs:
+%       allData:    a matrix, ntrain by nftrs
+%       isab:       flag abnormal cases, ndata by 1
+%
+%   Ouputs:
+%       traindataNor                data to be used for training
+%       validdataNor, validdataAb   data to be used for validation
+%       testdataNor, testdataAb     data to be used for testing
+%
+%   See also demoND
+
+normaldata = alldata(~isab, :);
+abnormaldata = alldata(isab, :);
+
+numdata = size(alldata, 1);
+numAb = sum(isab);
+numNor = numdata - numAb;
+
+
+% fprintf('%d \n', numdata);
+% fprintf('%d \n', numAb);
+% fprintf('%d \n', numNor);
+
+if en_ae == 1 
+    traindataNor = normaldata(1:7000, :);
+    testdataNor = normaldata(7001:10000, :);
+    testdataAb = abnormaldata(1:3000, :);
+    validdataAb = abnormaldata(3000:end, :);
+    validdataNor = normaldata(10001:10000+size(validdataAb,1), :);
+else
+    traindataNor = normaldata(1:700, :);
+    testdataNor = normaldata(701:1190, :);
+    testdataAb = abnormaldata(1:490, :);
+    validdataAb = abnormaldata(491:492, :);
+    validdataNor = normaldata(1191:1190+size(validdataAb,1), :);
+end 
+
+% permNor = randperm(numNor);
+% indTrainNor = permNor(1:7000);
+% traindataNor = normaldata(indTrainNor, :);
+% 
+% 
+% indTestNor = permNor(7001:10000);
+% % indValidNor = permNor(10001:10159);
+% testdataNor = normaldata(indTestNor, :);
+% permAb = randperm(numAb);
+% indTestAb = permAb(1:3000);
+% testdataAb = abnormaldata(indTestAb, :);
+% 
+% 
+% indValidAb = permAb(3001:end);
+% validdataAb = abnormaldata(indValidAb, :);
+% indValidNor = permNor(10001: 10000+size(validdataAb,1));
+% validdataNor = normaldata(indValidNor, :);
+
+%%
+% if numNor > numAb * 2
+%     howtosplit = 'balance'; % this is not that correct, used in my AUC paper in Poland conference.
+%     % howtosplit = 'balanceByPts'; % this is the proper way; see BSP log 1.133. Implemented in RunND_byPts.m.
+% else
+%     howtosplit = 'percentage';
+% end
+% 
+% switch lower(howtosplit)
+%     case 'percentage'
+%         percentTrainNor = 0.6; % use 60% normal data for training.
+%         percentValidNor = 0.2; % use 20% normal data for validation => use 20% normal data for testing.
+%         numTrainNor = floor(percentTrainNor * numNor);
+%         numValidNor = floor(percentValidNor * numNor);
+%         numTestNor = numNor - numTrainNor - numValidNor;
+%         
+%         indValidAb = (1:numValidAb);
+%         indTestAb = (numValidAb+1 : numAb);
+%         
+%         indTrainNor = (1 : numTrainNor);
+%         indValidNor = (numTrainNor + 1 : numTrainNor + numValidNor);
+%         indTestNor = (numTrainNor + numValidNor + 1 : numNor);
+%         
+%     case 'balance' % Use the same number of normal data for validation and test, in order to balance the data set.
+%         numValidNor = numValidAb;
+%         numTestNor = numTestAb;
+%         numTrainNor = numNor - numValidNor - numTestNor;
+%         
+%         permAb = randperm(numAb);
+%         indValidAb = permAb(1:numValidAb);
+%         indTestAb = permAb(numValidAb+1 : end);
+%         
+%         permNor = randperm(numNor);
+%         indTrainNor = permNor(1 : numTrainNor);
+%         indValidNor = permNor(numTrainNor + 1 : numTrainNor + numValidNor);
+%         indTestNor = permNor(numTrainNor + numValidNor + 1 : end);
+% end
+
+%%
+% normaldata = alldata(~isab, :);
+% abnormaldata = alldata(isab, :);
+% % Find training, validation and test data
+% traindataNor = normaldata(indTrainNor, :); % only use normal data for training.
+% validdataAb = abnormaldata(indValidAb, :);
+% testdataAb = abnormaldata(indTestAb, :);
+% validdataNor = normaldata(indValidNor, :);
+% testdataNor = normaldata(indTestNor, :);
+
+end
